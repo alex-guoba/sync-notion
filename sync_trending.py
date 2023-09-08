718e23375ce0e73b5f7b10faefe4c821897bc0f8
alex.guoba <alex.guoba@gmail.com>
2023-09-08 17:52:33 +0800

    feat: add git repo infomation, include forks/watchers/stargazers count

--- sync_trending.py
+++ sync_trending.py
@@ -5,6 +5,8 @@ import logging
 import time
 import requests
 from pyquery import PyQuery as pq
+from github import Github
+from github import Auth
 
 from notion_client import Client
 
@@ -16,6 +18,33 @@ class TrendItem:
         self.title = title
         self.url = url
         self.desc = desc
+        self.watchers_count = 0
+        self.forks_count = 0
+        self.stargazers_count = 0
+
+    def _repo_path(self) -> str:
+        items = self.url.split('/')[-2:]
+        return '/'.join(items)
+
+    def fullfill_repo_info(self, git_token):
+        "fullfill basic info from repo"
+        if not self.url:
+            return
+        auth = None
+        if git_token:
+            auth = Auth.Token(git_token)
+        git = Github(auth=auth)
+
+        try:
+            repo = git.get_repo(self._repo_path())
+        # pylint: disable-next=broad-except
+        except Exception as _e:
+            logging.error("get repo %s error: %s", self._repo_path() , _e)
+            return
+
+        self.watchers_count = repo.watchers_count
+        self.forks_count = repo.forks_count
+        self.stargazers_count = repo.stargazers_count
 
 def query_page(client: Client, database_id: str, title: str) -> bool:
     """检查是否已经插入过 如果已经插入了就忽略"""
@@ -35,6 +64,7 @@ def query_page(client: Client, database_id: str, title: str) -> bool:
 
 
 def insert_page(client: Client, database_id: str, language: str, trend: TrendItem) -> None | str:
+    '''插入page'''
     parent = {
         "database_id": database_id,
         "type": "database_id"
@@ -42,16 +72,18 @@ def insert_page(client: Client, database_id: str, language: str, trend: TrendIte
     properties = {
         "Title": {"title": [{"type": "text", "text": {"content": trend.title}}]},
         "Language": {"select": {"name": language}},
-        #properties["Status"] = {"select": {
-        #    "name": "读完" if marked_status == 4 else "在读"}}
         "URL": {"url": trend.url},
         "Desc": {"rich_text": [{"type": "text", "text": {"content": trend.desc}}]},
+        "WatchersCount": {"number": trend.watchers_count},
+        "ForksCount": {"number": trend.forks_count},
+        "StargazersCount": {"number": trend.stargazers_count},
     }
     response = client.pages.create(parent=parent, properties=properties)
     return response["id"]
 
 def _scrape(language: str) -> list[TrendItem]:
     headers = {
+        # pylint: disable=line-too-long
         'User-Agent'		: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:11.0) Gecko/20100101 Firefox/11.0',
         'Accept'			: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
         'Accept-Encoding'	: 'gzip,deflate,sdch',
@@ -79,20 +111,25 @@ def _scrape(language: str) -> list[TrendItem]:
         url = "https://github.com" + i(".lh-condensed a").attr("href")
 
         result.append(TrendItem(title, url, description))
+
     return result
 
-def _sync(client: Client, database_id: str, language: str, trends: list[TrendItem]) -> None:
+# pylint: disable=line-too-long
+def _sync(client: Client, database_id: str, language: str, trends: list[TrendItem], git_token: str) -> None:
     for trend in trends:
-        time.sleep(0.3)
-
+        time.sleep(0.3) # avoid rate limit for notion API
         if query_page(client, database_id, trend.title):
             continue
         # insert to db
         logging.info(trend)
+
+        if git_token:
+            trend.fullfill_repo_info(git_token)
+
         insert_page(client, database_id, language, trend)
 
 
-def sync_trending(notion_token, database_id):
+def sync_trending(notion_token, database_id, git_token=None):
     """同步github trending到notion"""
     client = Client(
         auth=notion_token,
@@ -100,7 +137,6 @@ def sync_trending(notion_token, database_id):
     )
 
     languages = list(map(lambda x: x.strip(), CONFIG.get("trending.language", "languages").split(",")))
-    print(languages)
     for language in languages:
         if not language:
             continue
@@ -112,4 +148,4 @@ def sync_trending(notion_token, database_id):
             logging.error("language [%s] error", language)
             continue
 
-        _sync(client, database_id, language, trends)
+        _sync(client, database_id, language, trends, git_token)
